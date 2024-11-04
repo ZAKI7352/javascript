b4581e754ee844aebd3859a157f95f144a840b6c
Zaki7352 <mdzaki7352@gmail.com>
2024-11-04 18:02:58 +0530

    Data Type Summery

--- 01_basic/dataType_Summary.js
+++ 01_basic/dataType_Summary.js
@@ -24,8 +24,8 @@ console.log(id === anotherId);
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
-    age: 22,
+    name: "Zaki",
+    age: 25,
 }
 
 const myFunction = function(){
